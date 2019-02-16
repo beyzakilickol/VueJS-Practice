1bc4845eb871712ff1111f7030d39a63794f4b13
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2019-02-16 12:58:11 -0600

    set up, events, dynamic css

---
+++ index.js
@@ -0,0 +1,54 @@
+new Vue({
+    el: '#app',
+    data:{
+        title: 'Hello World',
+        link: 'http://google.com',
+        htmlTag: '<a href="http://google.com">Google</a>'
+    },
+    methods:{
+        changeTitle: function(e){
+            this.title=e.target.value;
+        },
+        sayHello: function (){
+            return this.title
+        }
+    }
+})
+new Vue({
+    el:'#counter',
+    data:{
+        counter: 0,
+
+    },
+    methods:{
+        increment: function(countby){
+           
+            this.counter += countby
+        },
+        alertMe: function(){
+            alert('Alert')
+        }
+    }
+})
+new Vue({
+    el:'#dynamicCSS',
+    data: {
+        attachedRed: false,
+        color: 'green',
+        width: 100
+    },
+    computed:{
+        divClasses: function (){
+            return{
+                red: this.attachedRed,
+                blue: !this.attachedRed
+            }
+        },
+        myStyle: function (){
+            return{
+                backgroundColor: this.color,
+                width: this.width + "px"
+            }
+        }
+    }
+})
